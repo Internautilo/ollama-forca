7cec52d3b492172a1dd7ffd2f5afe474eeb6ee2a
Internautilo <yuribertulucci2004@gmail.com>
2024-10-18 20:19:28 -0300

    refactor: minor fixes

--- resources/views/components/layout/footer.blade.php
+++ resources/views/components/layout/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="main-footer">
     <!-- To the right -->
     <div class="float-right d-none d-sm-inline">
-        Projeto Tópicos Especias 1
+        Projeto Tópicos Especias I
     </div>
     <!-- Default to the left -->
     <strong>Built with <a href="https://www.laravel.com">Laravel</a>/<a href="https://livewire.laravel.com">Livewire</a>/<a href="https://ollama.com/">Ollama</a> || </strong> All rights reserved.
