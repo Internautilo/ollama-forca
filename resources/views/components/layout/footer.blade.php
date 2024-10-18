a83a4195f0b2bb960154866adf5cbd65bc9de689
Internautilo <yuribertulucci2004@gmail.com>
2024-10-18 20:14:54 -0300

    feat: add footer component

---
+++ resources/views/components/layout/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+        Projeto Tópicos Especias 1
+    </div>
+    <!-- Default to the left -->
+    <strong>Built with <a href="https://www.laravel.com">Laravel</a>/<a href="https://livewire.laravel.com">Livewire</a>/<a href="https://ollama.com/">Ollama</a> || </strong> All rights reserved.
+</footer>
