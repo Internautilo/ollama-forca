21b64565029d7b58e717d38e6269a66f506d548c
Yuri N Bertulucci <yuribertulucci2004@gmail.com>
2024-10-19 12:36:41 -0300

    style: fix responsivity

--- resources/views/livewire/pages/new-game-form.blade.php
+++ resources/views/livewire/pages/new-game-form.blade.php
@@ -1,5 +1,5 @@
 <div class="container align-items-center">
-    <hr class="hr hr invisible" style="height: 20vh"/>
+    <hr class="hr hr invisible"/>
     <div class="card card-primary">
         <div class="card-header">
             <h3 class="card-title">Criação de Jogo</h3>
@@ -8,12 +8,11 @@
             <div class="card-body">
                 <div class="form-group">
                     <label for="tema">Tema do Jogo</label>
-                    <textarea class="form-control" id="tema" placeholder="Digite um tema para a palavra do jogo"></textarea>
+                    <textarea class="form-control" id="tema" name="tema" placeholder="Digite um tema para a palavra do jogo"></textarea>
                 </div>
             </div>
-
             <div class="card-footer d-flex justify-content-center align-items-center">
-                <button type="submit" class="btn btn-primary" style="width: 30%">Criar Jogo</button>
+                <button type="submit" class="btn btn-primary">Iniciar Jogo</button>
             </div>
         </form>
     </div>
