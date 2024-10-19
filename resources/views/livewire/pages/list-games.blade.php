01416a408fa9b1e309a4f29c5b4b90364435596f
Yuri N Bertulucci <yuribertulucci2004@gmail.com>
2024-10-19 17:23:16 -0300

    style: fix responsivity

--- resources/views/livewire/pages/list-games.blade.php
+++ resources/views/livewire/pages/list-games.blade.php
@@ -1,3 +1,27 @@
+@php
+
+function calcPercentage(string $word, string $correctLetters, bool $descriptive = false): float|string
+{
+    $letrasCorretas = 0;
+    $correctLettersArray = explode(',', $correctLetters);
+    $uniqueLetters = array_unique(array_map('strtolower', $correctLettersArray));
+
+    foreach ($uniqueLetters as $letter) {
+        $letrasCorretas += substr_count(strtolower($word), strtolower($letter));
+    }
+
+    if ($descriptive) {
+        return $letrasCorretas . ' / ' . strlen($word);
+    }
+
+    if (strlen($word) > 0) {
+        $response = ($letrasCorretas / strlen($word)) * 100;
+        return number_format($response, 2);
+    }
+    return 0;
+}
+
+@endphp
 
 <div class="container">
     <div class="card">
@@ -13,7 +37,8 @@
                 </div>
                 <div class="row">
                     <div class="col-sm-12">
-                        <table id="example2" class="table table-bordered table-hover table-striped dataTable dtr-inline collapsed"
+                        <table id="example2"
+                               class="table table-bordered table-hover dataTable dtr-inline collapsed"
                                aria-describedby="example2_info">
                             <thead>
                             <tr>
@@ -25,7 +50,7 @@
                                     aria-label="Browser: activate to sort column ascending">Palavra
                                 </th>
                                 <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1"
-                                    aria-label="Platform(s): activate to sort column ascending">Conclusão
+                                    aria-label="Platform(s): activate to sort column ascending">Progresso
                                 </th>
                                 <th></th>
                             </tr>
@@ -33,12 +58,22 @@
                             <tbody>
                             @foreach($games as $game)
                                 <tr class="odd">
-                                    <td class="dtr-control sorting_1" tabindex="0">{{ $game->theme }}</td>
+                                    <td class="" tabindex="0">{{ $game->theme }}</td>
                                     <td>{{ $game->keyword }}</td>
-                                    <td>{{ $game->correct_letters }}</td>
+                                    <td>
+                                        <div class="progress mb-2">
+                                            <div class="progress-bar bg-danger" role="progressbar"
+                                                 style="width: {{ calcPercentage($game->keyword, 'a,b,c') }}%">
+                                            </div>
+                                        </div>
+                                        {{ calcPercentage($game->keyword, 'a,b,c', true) }} Letras Encontradas
+                                    </td>
                                     <td class="d-flex justify-content-around align-items-center">
-                                        <a style="width: 40%" class="btn btn-light" wire:navigate href="{{ route("game", [$id => $game->id]) }}"><i class="fas fa-play"></i></a>
-                                        <a style="width: 40%" class="btn btn-danger" wire:navigate><i class="fas fa-trash"></i></a>
+                                        <a style="width: 45%" class="btn btn-default" wire:navigate
+                                           href="{{ route("game", ['id' => $game->id]) }}"><i
+                                                class="fas fa-play"></i></a>
+                                        <a style="width: 45%" class="btn btn-danger" wire:navigate><i
+                                                class="fas fa-trash"></i></a>
                                     </td>
                                 </tr>
                             @endforeach
@@ -48,44 +83,45 @@
                 </div>
             </div>
         </div>
-
     </div>
 </div>
 
 @script
 <script>
-    $(function () {
-        $('#example2').DataTable({
-            "paging": true,
-            "lengthChange": false,
-            "searching": false,
-            "ordering": true,
-            "info": true,
-            "autoWidth": false,
-            "responsive": true,
-            "columnDefs": [
-                { "orderable": false, "targets": 3 }
-            ],
-            "language": {
-                "emptyTable": "Nenhum registro encontrado",
-                "info": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
-                "infoFiltered": "(Filtrados de _MAX_ registros)",
-                "infoThousands": ".",
-                "loadingRecords": "Carregando...",
-                "zeroRecords": "Nenhum registro encontrado",
-                "search": "Pesquisar",
-                "paginate": {
-                    "next": "Próximo",
-                    "previous": "Anterior",
-                    "first": "Primeiro",
-                    "last": "Último"
-                },
-                "aria": {
-                    "sortAscending": ": Ordenar colunas de forma ascendente",
-                    "sortDescending": ": Ordenar colunas de forma descendente"
-                },
+    if ($('.dataTables_info')) {
+        $('.dataTables_info').parent().parent().remove();
+    }
+    $('#example2').DataTable({
+        "paging": false,
+        "lengthChange": false,
+        "searching": false,
+        "ordering": true,
+        "info": false,
+        "autoWidth": false,
+        "responsive": false,
+        "scrollX": true,
+        "columnDefs": [
+            {"orderable": false, "targets": 3}
+        ],
+        "language": {
+            "emptyTable": "Nenhum registro encontrado",
+            "info": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+            "infoFiltered": "(Filtrados de _MAX_ registros)",
+            "infoThousands": ".",
+            "loadingRecords": "Carregando...",
+            "zeroRecords": "Nenhum registro encontrado",
+            "search": "Pesquisar",
+            "paginate": {
+                "next": "Próximo",
+                "previous": "Anterior",
+                "first": "Primeiro",
+                "last": "Último"
             },
-        });
+            "aria": {
+                "sortAscending": ": Ordenar colunas de forma ascendente",
+                "sortDescending": ": Ordenar colunas de forma descendente"
+            },
+        },
     });
 </script>
 @endscript
