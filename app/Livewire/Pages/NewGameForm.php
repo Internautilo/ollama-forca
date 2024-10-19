af506b986068c7454911df81868924799046b02b
Yuri N Bertulucci <yuribertulucci2004@gmail.com>
2024-10-19 20:15:35 -0300

    feat: add save function (unstested)

--- app/Livewire/Pages/NewGameForm.php
+++ app/Livewire/Pages/NewGameForm.php
@@ -2,12 +2,41 @@
 
 namespace App\Livewire\Pages;
 
+use App\Http\Controllers\Api\OllamaApi;
+use App\Models\Game;
 use Livewire\Component;
 
 class NewGameForm extends Component
 {
+    public string $theme = '';
+
     public function render()
     {
         return view('livewire.pages.new-game-form');
+    }
+
+    /**
+     *
+     * @throws \Exception
+     */
+    public function save(): void
+    {
+        if (!auth()->check()) {
+            throw new \Exception('User is not authenticated');
+        }
+
+        if ($this->theme !== '') {
+            try {
+                $response = OllamaApi::Prompt($this->theme);
+                $gameId = Game::create([
+                    'keyword' => $response,
+                    'user_id' => auth()->id(),
+                ]);
+
+                $this->dispatch('game-created', ['gameId' => $gameId]);
+            } catch (\Exception $e) {
+                throw new \Exception("Failed to connect to the API: " . $e->getMessage());
+            }
+        }
     }
 }
