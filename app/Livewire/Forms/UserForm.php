551371be5424100d368c1881a367b9638c9ae842
Internautilo <yuribertulucci2004@gmail.com>
2024-10-25 19:40:46 -0300

    feat: add custom messages to validation

--- app/Livewire/Forms/UserForm.php
+++ app/Livewire/Forms/UserForm.php
@@ -24,4 +24,24 @@ class UserForm extends Form
         User::create($this->all());
         redirect()->route('home');
     }
+
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:8',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'O campo nome é obrigatório',
+            'email.required' => 'O campo e-mail é obrigatório',
+            'email.email' => 'Digite um e-mail válido',
+            'password.required' => 'O campo senha é obrigatório',
+            'password.min' => 'A senha deve ter ao menos 8 caracteres',
+        ];
+    }
 }
